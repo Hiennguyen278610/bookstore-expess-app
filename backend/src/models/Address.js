9c89ce095788801f4cbb25fe5951628a98cecac9
Phung Thanh Do <hoangquy3001@gmail.com>
2025-12-05 23:16:56 +0700

    feat: add maps

--- backend/src/models/Address.js
+++ backend/src/models/Address.js
@@ -6,4 +6,6 @@ const addressSchema = new mongoose.Schema({
   address: { type: String, required: true },
   type: { type: String, required: true },
   isDefault: { type: Boolean, default: false },
-})
+}, {timestamps: true})
+
+export default mongoose.model('Address', addressSchema);
