ddd3194147d6f7ac051adbebde77e99d4e2d3d4a
fong <keikan010822@gmail.com>
2025-12-07 13:57:48 +0700

    cart

--- backend/src/controllers/CartController.js
+++ backend/src/controllers/CartController.js
@@ -1,12 +1,18 @@
-import User from '../models/User.js';
-import { addItemToCart, clearCartService, removeItemFromCart, updateItemQuantity } from '../services/CartService.js';
+import User from "../models/User.js";
+import {
+  addItemToCart,
+  clearCartService,
+  getCartService,
+  removeItemFromCart,
+  updateItemQuantity,
+} from "../services/CartService.js";
 
 export async function addItem(req, res) {
   try {
     const { bookId, quantity } = req.body;
     const cart = await addItemToCart(bookId, req.user.id, quantity);
     if (!cart) {
-      return res.status(401).json({ message: 'Cart not found' });
+      return res.status(401).json({ message: "Cart not found" });
     }
     return res.status(200).json(cart);
   } catch (err) {
@@ -16,10 +22,10 @@ export async function addItem(req, res) {
 
 export async function removeItem(req, res) {
   try {
-    const { bookId } = req.body;
-    const cart = await removeItemFromCart(bookId, req.user.id);
+    const { id } = req.params;
+    const cart = await removeItemFromCart(id, req.user.id);
     if (!cart) {
-      return res.status(401).json({ message: 'Cart not found' });
+      return res.status(401).json({ message: "Cart not found" });
     }
     return res.status(200).json(cart);
   } catch (err) {
@@ -29,10 +35,11 @@ export async function removeItem(req, res) {
 
 export async function updateQuantity(req, res) {
   try {
-    const { bookId, quantity } = req.body;
-    const cart = await updateItemQuantity(bookId, req.user.id, quantity);
+    const { id } = req.params;
+    const { quantity } = req.body;
+    const cart = await updateItemQuantity(id, req.user.id, quantity);
     if (!cart) {
-      return res.status(401).json({ message: 'Cart not found' });
+      return res.status(401).json({ message: "Cart not found" });
     }
     return res.status(200).json(cart);
   } catch (err) {
@@ -44,7 +51,19 @@ export async function clearCart(req, res) {
   try {
     const cart = await clearCartService(req.user.id);
     if (!cart) {
-      return res.status(401).json({ message: 'Cart not found' });
+      return res.status(401).json({ message: "Cart not found" });
+    }
+    return res.status(200).json(cart);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+}
+
+export async function getCart(req, res) {
+  try {
+    const cart = await getCartService(req.user.id);
+    if (!cart) {
+      return res.status(401).json({ message: "Cart not found" });
     }
     return res.status(200).json(cart);
   } catch (err) {
--- backend/src/routes/CartRouters.js
+++ backend/src/routes/CartRouters.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addItem, clearCart, removeItem, updateQuantity } from '../controllers/CartController.js';
+import { addItem, clearCart, getCart, removeItem, updateQuantity } from '../controllers/CartController.js';
 import { auth } from '../middlewares/auth.js';
 import { checkEmptyBody } from '../middlewares/checkEmptyBody.js';
 
@@ -9,9 +9,10 @@ const router = express.Router();
 router.use(auth)
 router.use(checkEmptyBody)
 
+router.get("/", getCart)
 router.post("/", addItem);
-router.put("/", updateQuantity) // chua test
-router.delete("/:id", removeItem) // chua test
-router.delete("/", clearCart) // chua test
+router.put("/:id", updateQuantity) // :id = cartDetailId
+router.delete("/:id", removeItem) // :id = cartDetailId
+router.delete("/", clearCart) 
 
 export default router;
