7b6c863f8382c85a466cfd16da19a6b6c5dc6e17
Phung Thanh Do <hoangquy3001@gmail.com>
2025-12-10 23:39:42 +0700

    update: banner

--- frontend/src/app/(home)/page.tsx
+++ frontend/src/app/(home)/page.tsx
@@ -1,17 +1,33 @@
-
-import { testCardBooks } from "@/constants/user.index";
+"use client"
 import BannerSlider from "./components/Banner";
-import BookShowCase from "./components/BookShowCase";
 import CategorySidebar from "./components/CategorySidebar";
 import BrandCarousel from "./components/BrandCarousel";
+import BookShowCase from '@/app/(home)/components/BookShowCase';
+import BookNewest from '@/app/(home)/components/BookNewest';
+import { getAllTop10BestSellingBooks, getAllTop10NewestBooks } from '@/hooks/useBook';
 
 export default function HomePage() {
+
+  const { bestSelling, isLoading: Selling } = getAllTop10BestSellingBooks()
+  const { newest, isLoading: Newest } = getAllTop10NewestBooks()
+
+  if (Selling || Newest) return <div className="text-center py-4 text-sm text-gray-500">Đang tải thông tin sách...</div>;
+
   return (
-    <div className="w-full max-w-[1200px] p-2 mt-4">
-      <div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-1">
+    <div className="w-full max-w-[1200px] p-2 mt-4 mx-auto">
+
+      {/* KHU VỰC TRÊN: Sidebar và Banner nằm ngang */}
+      <div className="grid grid-cols-1 lg:grid-cols-[1fr_2fr] gap-1 mb-8">
         <CategorySidebar />
         <BannerSlider />
       </div>
+
+      {/* KHU VỰC DƯỚI: Các section sách nằm dọc (Full width) */}
+      <div className="flex flex-col gap-8">
+        <BookShowCase title={"Sách bán chạy"} books={bestSelling} />
+        <BookNewest title={"Sách mới nhất"} books={newest} />
+      </div>
+
       <BrandCarousel />
     </div>
   );
--- frontend/src/types/book.type.ts
+++ frontend/src/types/book.type.ts
@@ -2,14 +2,19 @@ import { Author } from "./author.type";
 import { Category } from "./category.type";
 import { Publisher } from "./publisher.type";
 
-export interface Book_2 {
-  id: string;
+export interface BookDetail {
+  _id: string;
   name: string;
-  category_id: string;
-  publisher_id: string;
-  imageUrl: string;
+  categoryId: Category;
+  publisherId: Publisher;
+  imageUrl: [string]
   quantity: number;
   price: number;
+}
+export interface BookBanner {
+  _id: string;
+  totalSold: string;
+  book: BookDetail;
 }
 
 
