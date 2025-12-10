e428891ef71d8458ce3b6a060d8fb3ae25798ba5
Phung Thanh Do <hoangquy3001@gmail.com>
2025-12-10 10:59:24 +0700

    add: method COD CARD, Admin switch

--- backend/src/routes/AuthorRouters.js
+++ backend/src/routes/AuthorRouters.js
@@ -6,9 +6,9 @@ import { checkEmptyBody } from '../middlewares/checkEmptyBody.js';
 
 const router = express.Router();
 
-//router.use(auth)
-//router.use(authorizeRoles("admin"))
-//router.use(checkEmptyBody)
+router.use(auth)
+router.use(authorizeRoles("admin"))
+router.use(checkEmptyBody)
 router.post("/", createAuthor);
 router.put("/:id", updateAuthor);
 router.get("/:id", getAuthorById);
--- frontend/src/app/admin/layout.tsx
+++ frontend/src/app/admin/layout.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
-import { Toaster } from "react-hot-toast";
-import { Toaster as ToastSonner } from "sonner";
+import { toast, Toaster } from 'sonner';
+import { useUser } from '@/services/authservices';
+import { useRouter } from 'next/navigation';
 
 
 export default function AdminLayout({
@@ -12,30 +13,44 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   const [isOpen, setIsOpen] = useState(true);
+  const {user, isLoading} = useUser()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (!isLoading) {
+      if (!user) {
+        toast.error("Vui lòng đăng nhập để truy cập");
+        router.push("/");
+        return;
+      }
+      if (user.data.role !== "admin") {
+        toast.warning("Bạn không có quyền truy cập trang này");
+        router.push("/");
+      }
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading || !user || user.data.role !== "admin") {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
 
   return (
     <div className="flex bg-gray-100 min-h-screen">
-      <Toaster
-        position="top-center"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            fontSize: '16px',
-            minWidth: '300px',
-            padding: '16px',
-          },
-        }}
-      />
       <Sidebar isOpen={isOpen} />
       <div
         className={`flex-1 transition-all duration-700 ${isOpen ? "ml-64" : "ml-18"
           }`}
       >
         <Navbar toggleSidebar={() => setIsOpen(!isOpen)} isOpen={isOpen} />
-        <ToastSonner richColors />
         <main className="p-6 mt-16 transition-all duration-700 bg-gray-100">
           {children}
         </main>
+        <Toaster />
       </div>
     </div>
   );
